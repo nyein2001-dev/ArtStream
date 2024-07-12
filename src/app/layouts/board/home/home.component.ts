5f0a6cd1708fe4fc7fe720fc1072498ae673518d
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-13 02:12:39 +0630

    complete main screen

--- src/app/layouts/board/home/home.component.ts
+++ src/app/layouts/board/home/home.component.ts
@@ -6,6 +6,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  artists: any[] = [
+    {
+      name: 'Thomas Edward',
+      username: 'thewildwithyou',
+      imageUrl: 'assets/img/image1.png',
+      profileImageUrl: 'assets/img/img1.png'
+    },
+    {
+      name: 'Chris Doe',
+      username: 'thewildwithyou',
+      imageUrl: 'assets/img/image2.png',
+      profileImageUrl: 'assets/img/img2.png'
+    },
+    {
+      name: 'Emilie Jones',
+      username: 'thewildwithyou',
+      imageUrl: 'assets/img/image3.png',
+      profileImageUrl: 'assets/img/img3.png'
+    },
+    {
+      name: 'Jessica Williams',
+      username: 'thewildwithyou',
+      imageUrl: 'assets/img/image4.png',
+      profileImageUrl: 'assets/img/img4.png'
+    },
+    {
+      name: 'Lara Leones',
+      username: 'thewallart',
+      imageUrl: 'assets/img/image5.png',
+      profileImageUrl: 'assets/img/img5.png'
+    },
+    {
+      name: 'Thomas J.',
+      username: 'thecustomcreater',
+      imageUrl: 'assets/img/news1.png',
+      profileImageUrl: 'assets/img/profile1.png'
+    },
+    {
+      name: 'John Doe',
+      username: 'thewildwithyou',
+      imageUrl: 'assets/img/news2.png',
+      profileImageUrl: 'assets/img/profile2.png'
+    },
+    {
+      name: 'Jane Smith',
+      username: 'thewildwithyou',
+      imageUrl: 'assets/img/image2.png',
+      profileImageUrl: 'assets/img/img2.png'
+    }
+  ];
+
+
   posts: any[] = [
     {
       name: 'Thomas Edward',
