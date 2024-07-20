0131de383387a637f6a435597ef6aa8d37eecbaf
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-20 08:42:12 +0630

    setup icon folder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { AppRoutingModule } from './app/app-routing.module';
