308477e6ac7db9f0bb199f85707c345a56ce9c6f
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-20 13:58:13 +0630

    refactor code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
-  },
-  {
-    path: 'auth',
-    loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  },
-  {
-    path: 'errors',
-    loadChildren: () => import('./modules/error/error.module').then((m) => m.ErrorModule),
-  }
-];
+import { routes } from './app.routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
