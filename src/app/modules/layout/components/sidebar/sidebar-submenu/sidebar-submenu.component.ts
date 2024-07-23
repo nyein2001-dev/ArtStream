e221c294c16967b35c47a1fbd9974e44a6506de1
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-23 20:40:44 +0630

    add sidebar submenu

--- src/app/modules/layout/components/sidebar/sidebar-submenu/sidebar-submenu.component.ts
+++ src/app/modules/layout/components/sidebar/sidebar-submenu/sidebar-submenu.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { NgClass, NgFor, NgTemplateOutlet } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { SubMenuItem } from '../../../../../core/models/menu.model';
+import { MenuService } from '../../../services/menu.service';
 
 @Component({
   selector: 'app-sidebar-submenu',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    NgFor,
+    NgTemplateOutlet,
+    RouterLinkActive,
+    RouterLink,
+    AngularSvgIconModule,
+  ],
   templateUrl: './sidebar-submenu.component.html',
   styleUrl: './sidebar-submenu.component.scss'
 })
-export class SidebarSubmenuComponent {
+export class SidebarSubmenuComponent implements OnInit {
+  @Input() public submenu = <SubMenuItem>{};
 
+  constructor(public menuService: MenuService) { }
+
+  ngOnInit(): void { }
+
+  public toggleMenu(menu: any) {
+    this.menuService.toggleSubMenu(menu);
+  }
+
+  private collapse(items: Array<any>) {
+    items.forEach((item) => {
+      item.expanded = false;
+      if (item.children) this.collapse(item.children);
+    })
+  }
 }
