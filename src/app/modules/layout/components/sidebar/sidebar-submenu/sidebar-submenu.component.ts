e5680cc4257df67b5668fc6203450fc77e679722
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-23 14:20:16 +0630

    add sidebar menu

---
+++ src/app/modules/layout/components/sidebar/sidebar-submenu/sidebar-submenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-submenu',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar-submenu.component.html',
+  styleUrl: './sidebar-submenu.component.scss'
+})
+export class SidebarSubmenuComponent {
+
+}
