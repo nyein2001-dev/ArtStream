38d37e8e542bcded45d707ada4b7b71619a8e6f7
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-23 23:09:14 +0630

    navbar component initialize

---
+++ src/app/modules/layout/components/navbar/profile-menu/profile-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-menu.component.html',
+  styleUrl: './profile-menu.component.scss'
+})
+export class ProfileMenuComponent {
+
+}
--- src/app/modules/layout/services/menu.service.ts
+++ src/app/modules/layout/services/menu.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnDestroy, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuItem, SubMenuItem } from '../../../core/models/menu.model';
-import { single } from 'rxjs';
+import { retry, single } from 'rxjs';
 import { Menu } from '../../../core/constants/menu';
 
 @Injectable({
@@ -9,6 +9,7 @@ import { Menu } from '../../../core/constants/menu';
 })
 export class MenuService implements OnDestroy {
   private _showSidebar = signal(true);
+  private _showMobileMenu = signal(false);
   private _pagesMenu = signal<MenuItem[]>([]);
 
   constructor(private router: Router) {
@@ -19,12 +20,20 @@ export class MenuService implements OnDestroy {
     return this._showSidebar();
   }
 
+  get showMobileMenu() {
+    return this._showMobileMenu();
+  }
+
   get pagesMenu() {
     return this._pagesMenu();
   }
 
   set showSideBar(value: boolean) {
     this._showSidebar.set(value);
+  }
+
+  set showMobileMenu(value: boolean) {
+    this._showMobileMenu.set(value);
   }
 
   public toggleSidebar() {
