48451ce7e3090d81ea475a1f69d1b3ce97bd0474
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-24 10:29:45 +0630

    add navbar menu

--- src/app/modules/layout/components/navbar/navbar-menu/navbar-menu.component.ts
+++ src/app/modules/layout/components/navbar/navbar-menu/navbar-menu.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../../../services/menu.service';
+import { NavbarSubmenuComponent } from '../navbar-submenu/navbar-submenu.component';
+import { NgFor, NgClass } from '@angular/common';
+import { MenuItem } from '../../../../../core/models/menu.model';
 
 @Component({
-  selector: 'app-navbar-menu',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar-menu.component.html',
-  styleUrl: './navbar-menu.component.scss'
+    selector: 'app-navbar-menu',
+    templateUrl: './navbar-menu.component.html',
+    styleUrls: ['./navbar-menu.component.scss'],
+    standalone: true,
+    imports: [
+        NgFor,
+        NgClass,
+        NavbarSubmenuComponent,
+    ],
 })
-export class NavbarMenuComponent {
+export class NavbarMenuComponent implements OnInit {
+  private showMenuClass = ['scale-100', 'animate-fade-in-up', 'opacity-100', 'pointer-events-auto'];
+  private hideMenuClass = ['scale-95', 'animate-fade-out-down', 'opacity-0', 'pointer-events-none'];
+
+  constructor(public menuService: MenuService) {}
+
+  ngOnInit(): void {}
+
+  public toggleMenu(menu: MenuItem): void {
+    menu.selected = !menu.selected;
+  }
 
 }
---
+++ src/app/modules/layout/components/navbar/navbar-submenu/navbar-submenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-submenu',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-submenu.component.html',
+  styleUrl: './navbar-submenu.component.scss'
+})
+export class NavbarSubmenuComponent {
+
+}
