6e643f0370ed31d403781b0a03616b53ba10ccbb
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-25 15:55:52 +0630

    add navbar mobile

---
+++ src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobile-menu/navbar-mobile-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-mobile-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-mobile-menu.component.html',
+  styleUrl: './navbar-mobile-menu.component.scss'
+})
+export class NavbarMobileMenuComponent {
+
+}
