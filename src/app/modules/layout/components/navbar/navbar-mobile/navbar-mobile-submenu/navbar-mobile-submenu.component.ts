3123b32b14b0ef5a46f8bf1a907d9c4186274621
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-25 17:01:40 +0630

    add navbar mobile sub menu

--- src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobile-submenu/navbar-mobile-submenu.component.ts
+++ src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobile-submenu/navbar-mobile-submenu.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { NgClass, NgFor, NgTemplateOutlet } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { SubMenuItem } from '../../../../../../core/models/menu.model';
+import { MenuService } from '../../../../services/menu.service';
 
 @Component({
   selector: 'app-navbar-mobile-submenu',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    NgFor,
+    NgTemplateOutlet,
+    RouterLinkActive,
+    RouterLink,
+    AngularSvgIconModule,
+  ],
   templateUrl: './navbar-mobile-submenu.component.html',
   styleUrl: './navbar-mobile-submenu.component.scss'
 })
-export class NavbarMobileSubmenuComponent {
+export class NavbarMobileSubmenuComponent implements OnInit {
+  @Input() public submenu = <SubMenuItem>{}
+
+  constructor(private menuService: MenuService) { }
+  
+  ngOnInit(): void { }
+
+  public toggleMenu(menu: any) {
+    this.menuService.toggleSubMenu(menu);
+  }
+
+  private collapse(items: Array<any>) {
+    items.forEach((item) => {
+      item.expanded = false;
+      if (item.children) this.collapse(item.children);
+    });
+  }
+
+  public closeMobileMenu() {
+    this.menuService.showMobileMenu = false;
+  }
 
 }
