5711db6b5b8fe12af923ef9c3e0e7ac3144ef97c
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-25 22:06:54 +0630

    add footer

--- src/app/modules/layout/components/footer/footer.component.ts
+++ src/app/modules/layout/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  public year: number = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void { }
 
 }
