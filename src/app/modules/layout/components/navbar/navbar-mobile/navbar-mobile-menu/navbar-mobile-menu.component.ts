3e3111d7aeeb8989f8e6b944c0b3d2c5b7146789
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-25 16:41:51 +0630

    add navbar mobile menu

--- src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobile-menu/navbar-mobile-menu.component.ts
+++ src/app/modules/layout/components/navbar/navbar-mobile/navbar-mobile-menu/navbar-mobile-menu.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { NgClass, NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { NavbarMobileSubmenuComponent } from '../navbar-mobile-submenu/navbar-mobile-submenu.component';
+import { MenuService } from '../../../../services/menu.service';
+import { SubMenuItem } from '../../../../../../core/models/menu.model';
 
 @Component({
   selector: 'app-navbar-mobile-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    NgClass,
+    AngularSvgIconModule,
+    NgTemplateOutlet,
+    RouterLink,
+    RouterLinkActive,
+    NgIf,
+    NavbarMobileSubmenuComponent,
+  ],
   templateUrl: './navbar-mobile-menu.component.html',
   styleUrl: './navbar-mobile-menu.component.scss'
 })
-export class NavbarMobileMenuComponent {
+export class NavbarMobileMenuComponent implements OnInit {
+
+  constructor(public menuService: MenuService) { }
+
+  public toggleMenu(subMenu: SubMenuItem) {
+    this.menuService.toggleMenu(subMenu);
+  }
+
+  public closeMenu() {
+    this.menuService.showMobileMenu = false;
+  }
+
+  ngOnInit(): void { }
 
 }
