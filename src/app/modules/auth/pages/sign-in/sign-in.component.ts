c99575a63390a4b6a12a3790a32adb63a5930eb6
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-26 15:54:42 +0630

    add button component

--- src/app/modules/auth/pages/sign-in/sign-in.component.ts
+++ src/app/modules/auth/pages/sign-in/sign-in.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink, AngularSvgIconModule, NgClass, NgIf],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+  form!: FormGroup;
+  submitted = false;
+  passwordTextType!: boolean;
+
+  constructor(private readonly _formBuilder: FormBuilder, private readonly _router: Router) { }
+
+  onClick() {
+    console.log('Button clicked');
+  }
+
+  ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  togglePasswordTextType() {
+    this.passwordTextType = !this.passwordTextType;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    const { email, password } = this.form.value;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this._router.navigate(['/']);
+  }
 
 }
---
+++ src/app/shared/components/responsive-helper/responsive-helper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responsive-helper',
+  standalone: true,
+  imports: [],
+  templateUrl: './responsive-helper.component.html',
+  styleUrl: './responsive-helper.component.scss'
+})
+export class ResponsiveHelperComponent {
+
+}
