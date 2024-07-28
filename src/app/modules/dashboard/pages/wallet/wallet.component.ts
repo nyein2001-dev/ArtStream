333fc794b60b9b092b60c44c0d938cfda78f12ee
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-28 16:02:02 +0630

    add wallet header

--- src/app/modules/dashboard/pages/wallet/wallet.component.ts
+++ src/app/modules/dashboard/pages/wallet/wallet.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { WalletHeaderComponent } from '../../components/wallet/wallet-header/wallet-header.component';
 
 @Component({
   selector: 'app-wallet',
   standalone: true,
-  imports: [],
+  imports: [WalletHeaderComponent],
   templateUrl: './wallet.component.html',
   styleUrl: './wallet.component.scss'
 })
