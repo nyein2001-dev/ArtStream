b257e7e655353e01ff955e08d5e72b13b8a6ddfb
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-28 11:48:00 +0630

    change route path

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -3,15 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { NftComponent } from './pages/nft/nft.component';
 import { PodcastComponent } from './pages/podcast/podcast.component';
+import { NotificationComponent } from './components/notification/notification.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
+import { WalletComponent } from './components/wallet/wallet.component';
+import { SubscriptionComponent } from './components/subscription/subscription.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SettingComponent } from './components/setting/setting.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
     children: [
-      { path: '', redirectTo: 'nfts', pathMatch: 'full' },
-      { path: 'nfts', component: NftComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: NftComponent },
       { path: 'podcast', component: PodcastComponent },
+      { path: 'notifications', component: NotificationComponent },
+      { path: 'shop', component: ShopComponent },
+      { path: 'conversation', component: ConversationComponent },
+      { path: 'wallet', component: WalletComponent },
+      { path: 'subscription', component: SubscriptionComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'setting', component: SettingComponent },
       { path: '**', redirectTo: 'errors/404' },
     ],
   },
@@ -21,4 +35,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class DashboardRoutingModule {}
+export class DashboardRoutingModule { }
