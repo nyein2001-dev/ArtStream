53af413567abf816d0043216293f46ae0c59326e
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-28 16:26:55 +0630

    add profile header

--- src/app/modules/dashboard/pages/profile/profile.component.ts
+++ src/app/modules/dashboard/pages/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProfileHeaderComponent } from '../../components/profile/profile-header/profile-header.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ProfileHeaderComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
