8159fe608da69ad03ec08187b4cdbd7cb5290c78
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-28 11:10:25 +0630

    enhance navbar

--- src/app/core/constants/menu.ts
+++ src/app/core/constants/menu.ts
@@ -3,85 +3,53 @@ import { MenuItem } from '../models/menu.model';
 export class Menu {
   public static pages: MenuItem[] = [
     {
-      group: 'Base',
-      separator: false,
-      items: [
-        {
-          icon: 'assets/icons/heroicons/outline/chart-pie.svg',
-          label: 'Dashboard',
-          route: '/dashboard',
-          children: [
-            { label: 'Nfts', route: '/dashboard/nfts' },
-            // { label: 'Podcast', route: '/dashboard/podcast' },
-          ],
-        },
-        {
-          icon: 'assets/icons/heroicons/outline/lock-closed.svg',
-          label: 'Auth',
-          route: '/auth',
-          children: [
-            { label: 'Sign up', route: '/auth/sign-up' },
-            { label: 'Sign in', route: '/auth/sign-in' },
-            { label: 'Forgot Password', route: '/auth/forgot-password' },
-            { label: 'New Password', route: '/auth/new-password' },
-            { label: 'Two Steps', route: '/auth/two-steps' },
-          ],
-        },
-        {
-          icon: 'assets/icons/heroicons/outline/shield-exclamation.svg',
-          label: 'Erros',
-          route: '/errors',
-          children: [
-            { label: '404', route: '/errors/404' },
-            { label: '500', route: '/errors/500' },
-          ],
-        },
-      ],
-    },
-    {
-      group: 'Collaboration',
+      group: '',
       separator: true,
       items: [
         {
-          icon: 'assets/icons/heroicons/outline/download.svg',
-          label: 'Download',
-          route: '/download',
+          icon: 'assets/icons/heroicons/outline/chart-pie.svg',
+          label: 'Home',
+          route: '/home',
         },
         {
-          icon: 'assets/icons/heroicons/outline/gift.svg',
-          label: 'Gift Card',
-          route: '/gift',
-        },
-        {
-          icon: 'assets/icons/heroicons/outline/users.svg',
-          label: 'Users',
-          route: '/users',
-        },
-      ],
-    },
-    {
-      group: 'Config',
-      separator: false,
-      items: [
-        {
-          icon: 'assets/icons/heroicons/outline/cog.svg',
-          label: 'Settings',
-          route: '/settings',
-        },
-        {
-          icon: 'assets/icons/heroicons/outline/bell.svg',
+          icon: 'assets/icons/heroicons/outline/notification.svg',
           label: 'Notifications',
-          route: '/gift',
+          route: '/notifications',
         },
         {
-          icon: 'assets/icons/heroicons/outline/folder.svg',
-          label: 'Folders',
-          route: '/folders',
-          children: [
-            { label: 'Current Files', route: '/folders/current-files' },
-            { label: 'Downloads', route: '/folders/download' },
-            { label: 'Trash', route: '/folders/trash' },
-          ],
+          icon: 'assets/icons/heroicons/outline/heart.svg',
+          label: 'Shop',
+          route: '/shop',
+        },
+        {
+          icon: 'assets/icons/heroicons/outline/conversation.svg',
+          label: 'Conversation',
+          route: '/conversation',
+        },
+        {
+          icon: 'assets/icons/heroicons/outline/wallet.svg',
+          label: 'Wallet',
+          route: '/wallet',
+        },
+        {
+          icon: 'assets/icons/heroicons/outline/favorite.svg',
+          label: 'Subscription',
+          route: '/subscription',
+        },
+        {
+          icon: 'assets/icons/heroicons/outline/profile.svg',
+          label: 'My Profile',
+          route: '/profile',
+        },
+        {
+          icon: 'assets/icons/heroicons/outline/setting.svg',
+          label: 'Settings',
+          route: '/setting',
+        },
+        {
+          icon: 'assets/icons/heroicons/outline/logout.svg',
+          label: 'Logout',
+          route: '/logout',
         },
       ],
     },
---
+++ src/app/modules/layout/components/seller/seller.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller',
+  standalone: true,
+  imports: [],
+  templateUrl: './seller.component.html',
+  styleUrl: './seller.component.scss'
+})
+export class SellerComponent {
+
+}
