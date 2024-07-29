c448d35b886eba08b2608cff2e1c2e72248c5fff
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-29 13:31:39 +0630

    remove filter widget

--- src/app/modules/layout/components/navbar/navbar.component.ts
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -2,26 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../../services/menu.service';
 import { NavbarMobileComponent } from './navbar-mobile/navbar-mobilecomponent';
 import { ProfileMenuComponent } from './profile-menu/profile-menu.component';
-import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { FilterComponent } from '../filter/filter.component';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
-    standalone: true,
-    imports: [
-        AngularSvgIconModule,
-        FilterComponent,
-        ProfileMenuComponent,
-        NavbarMobileComponent,
-    ],
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  standalone: true,
+  imports: [
+    AngularSvgIconModule,
+    ProfileMenuComponent,
+    NavbarMobileComponent,
+  ],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private menuService: MenuService) {}
+  constructor(private menuService: MenuService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   public toggleMobileMenu(): void {
     this.menuService.showMobileMenu = true;
